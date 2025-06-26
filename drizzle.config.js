826b434cdb7829e157402d64a8fd7b52f2ed9812
RayArena <chandreshsingh2902@gmail.com>
2025-06-26 22:36:13 +0530

    made some changes

--- drizzle.config.js
+++ drizzle.config.js
@@ -4,6 +4,6 @@ export default defineConfig({
   schema: "./configs/schema.js",
   out: "./drizzle",
   dbCredentials: {
-    url: 'postgresql://neondb_owner:npg_Lxl4PJtGcKN5@ep-silent-water-a83lo4kf-pooler.eastus2.azure.neon.tech/ai-short-video-generator?sslmode=require&channel_binding=require',
+    url: process.env.NEXT_PUBLIC_DRIZZLE_DATABASE_URL,
   },
 });
